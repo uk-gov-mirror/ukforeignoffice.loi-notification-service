c00507c7b7c39ba8f736ac45e9b13e3aa351b0b2
Sean <seanm@kainos.com>
2020-06-18 14:31:52 +0100

    update to code after breaking changes from dotenv package update

--- config/notify.js
+++ config/notify.js
@@ -3,9 +3,9 @@
  */
 var dotenv = require('dotenv');
 var env = dotenv.config({path: process.env.DOTENV || '.env'});
-var configs = JSON.parse(env.CONFIGS);
-var templates = JSON.parse(env.TEMPLATES);
-var urls = JSON.parse(env.URLS);
+var configs = JSON.parse(env.parsed.CONFIGS);
+var templates = JSON.parse(env.parsed.TEMPLATES);
+var urls = JSON.parse(env.parsed.URLS);
 var config = {configs: configs,
         templates:templates,
         urls:urls};
