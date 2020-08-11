a546dc7ebdc914ba73f69e0264f7262ca217b7b7
Trevor Rea <trevorr@kainos.com>
2020-08-11 14:02:18 +0100

    Swap out dotenv for simple environment variables

--- config/notify.js
+++ config/notify.js
@@ -1,11 +1,10 @@
 /**
  * Created by skaifem on 10/12/2015.
  */
-var dotenv = require('dotenv');
-var env = dotenv.config({path: process.env.DOTENV || '.env'});
-var configs = JSON.parse(env.parsed.CONFIGS);
-var templates = JSON.parse(env.parsed.TEMPLATES);
-var urls = JSON.parse(env.parsed.URLS);
+require('dotenv').config()
+var configs = JSON.parse(process.env.CONFIGS);
+var templates = JSON.parse(process.env.TEMPLATES);
+var urls = JSON.parse(process.env.URLS);
 var config = {configs: configs,
         templates:templates,
         urls:urls};
