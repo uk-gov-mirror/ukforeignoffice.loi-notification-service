c3bd794f78567b486d88389a382c244344bdac6a
Sean <seanm@kainos.com>
2021-03-03 11:03:20 +0000

    migrating sendgrid to notify

--- config/notify.js
+++ config/notify.js
@@ -1,13 +1,9 @@
-/**
- * Created by skaifem on 10/12/2015.
- */
-require('dotenv').config()
+var dotenv = require('dotenv');
+var env = dotenv.config({path: process.env.DOTENV || '.env'});
 var configs = JSON.parse(process.env.CONFIGS);
-var fromAddresses = JSON.parse(process.env.FROMADDRESSES);
 var templates = JSON.parse(process.env.TEMPLATES);
 var urls = JSON.parse(process.env.URLS);
 var config = {configs: configs,
-        fromAddresses:fromAddresses,
         templates:templates,
         urls:urls};
 module.exports = config;
